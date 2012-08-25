571b9c171e5f69ff2ed301c266335884880dfba9
Vojta Ciml <VojtaCiml@gmail.com>
2012-08-25 15:29:23 +0200

    experimenting with swfobject

--- javascript/dynamicalResizing.js
+++ javascript/dynamicalResizing.js
@@ -1,4 +1,9 @@
 // JavaScript Document
-window.setInterval(yourfunction, 1000);
+window.setInterval(yourfunction, 200);
+var currentSize = 10;
 
-function yourfunction() { alert('test'); }
+function yourfunction() { 
+	currentSize += 10;
+	console.log(currentSize);
+	$('#blockForResize').css({ width: currentSize, height: '300px' });
+}
