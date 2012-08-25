a07339cba09ea2f720aa62255b2d7b17e31372c9
Vojta Ciml <VojtaCiml@gmail.com>
2012-08-25 15:42:04 +0200

    JS experimenting

--- javascript/dynamicalResizing.js
+++ javascript/dynamicalResizing.js
@@ -1,9 +1,19 @@
 // JavaScript Document
-window.setInterval(yourfunction, 200);
+window.setInterval(yourFunction3, 200);
 var currentSize = 10;
 
 function yourfunction() { 
 	currentSize += 10;
 	console.log(currentSize);
 	$('#blockForResize').css({ width: currentSize, height: '300px' });
+}
+
+
+function yourFunction2() {
+	sendData(800);	
+}
+
+function sendData(val){
+	$('#blockForResize').css({ height: val });
+	console.log(val);	
 }
